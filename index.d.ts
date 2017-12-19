88037f01e54bd54bfbe9ca4dad0db239cbdf654d
yhpnorra <mafei.mf@alibaba-inc.com>
2017-12-19 19:56:50 +0800

    update code for new declare

--- index.d.ts
+++ index.d.ts
@@ -1,216 +1,185 @@
-// Definitions by: Alice <https://github.com/aaronphy>
+// Type definitions for mirrorx 0.2
+// Project: https://github.com/mirrorjs/mirror
+// Definitions by: Aaronphy <https://github.com/aaronphy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
 
+import * as H from 'history';
 
 import * as React from 'react';
-import * as H from 'history';
-import {
-  ReactInstance, Component, ComponentState,
-  ReactElement, SFCElement, CElement,
-  DOMAttributes, DOMElement, ReactNode, ReactPortal
-} from 'react';
-import {
-  History,
-  Location,
-  Path,
-  LocationState,
-  LocationDescriptor
-} from 'history';
+
 import { Connect } from 'react-redux';
 import { match } from "react-router";
 
+export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
 
-export = mirror;
-export as namespace mirror;
-
-declare namespace mirror {
-
-
-  type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]; 
-  type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
-
-  interface model {
-      name:string,
-      initialState?:any,
-      reducers?:any,
-      effects?:any
-  } 
-
-  interface _model {
-    name:string,
-    reducers:any
-  }
-
-  interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-    to: H.LocationDescriptor;
-    replace?: boolean;
-  }
-
-  interface NavLinkProps extends LinkProps {
-    activeClassName?: string;
-    activeStyle?: React.CSSProperties;
-    exact?: boolean;
-    strict?: boolean;
-    isActive?<P>(match: match<P>, location: H.Location): boolean;
-    location?: H.Location;
-  }
-
-  interface SwitchProps {
-    children?: React.ReactNode;
-    location?: H.Location;
-  }
-
-  interface RedirectProps {
-    to: H.LocationDescriptor;
-    push?: boolean;
-    from?: string;
-    path?: string;
-    exact?: boolean;
-    strict?: boolean;
-  }
-
-  interface RouteComponentProps<P> {
-    match: match<P>;
-    location: H.Location;
-    history: H.History;
-    staticContext?: any;
-  }
-
-  interface RouteProps {
-    location?: H.Location;
-    component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
-    render?: ((props: RouteComponentProps<any>) => React.ReactNode);
-    children?: ((props: RouteComponentProps<any>) => React.ReactNode) | React.ReactNode;
-    path?: string;
-    exact?: boolean;
-    strict?: boolean;
-  }
-
-  interface PromptProps {
-    message: string | ((location: H.Location) => string | boolean);
-    when?: boolean;
-  }
-
-  interface LocationActionPayload {
-    method: string;
-    args?: any[];
-  }
-
-  interface RouterAction {
-    type:'@@router/CALL_HISTORY_METHOD',
-    payload: LocationActionPayload;
-  }
-  
-  interface routeActions {
-    push(location:any,state?:any):RouterAction,
-    replace(location:any,state?:any):RouterAction,
-    go(n:number):RouterAction,
-    goBack(): RouterAction,
-    goForward(): RouterAction
-  }
-
-  export interface ConnectedRouterProps<State> {
-    store?: any;
-    history?: History;
+export interface model {
+  name: string;
+  initialState?: any;
+  reducers?: any;
+  effects?: any;
 }
 
-  interface Actions {
-    routing:routeActions,
-    [propName: string]: any
-  }
-
-  interface defaultOptions {
-    initialState?:{},
-    historyMode?:string,
-    middlewares:[any],
-    reducers:{},
-    addEffect(name:string,handler:()=>any):any
-  }
-
-  interface Renderer {
-    // Deprecated(render): The return value is deprecated.
-    // In future releases the render function's return type will be void.
-
-    <T extends Element>(
-        element: DOMElement<DOMAttributes<T>, T>,
-        container: Element | null,
-        callback?: () => void
-    ): T;
-
-    (
-        element: Array<DOMElement<DOMAttributes<any>, any>>,
-        container: Element | null,
-        callback?: () => void
-    ): Element;
-
-    (
-        element: SFCElement<any> | Array<SFCElement<any>>,
-        container: Element | null,
-        callback?: () => void
-    ): void;
-
-    <P, T extends Component<P, ComponentState>>(
-        element: CElement<P, T>,
-        container: Element | null,
-        callback?: () => void
-    ): T;
-
-    (
-        element: Array<CElement<any, Component<any, ComponentState>>>,
-        container: Element | null,
-        callback?: () => void
-    ): Component<any, ComponentState>;
-
-    <P>(
-        element: ReactElement<P>,
-        container: Element | null,
-        callback?: () => void
-    ): Component<P, ComponentState> | Element | void;
-
-    (
-        element: Array<ReactElement<any>>,
-        container: Element | null,
-        callback?: () => void
-    ): Component<any, ComponentState> | Element | void;
-
-    (
-        parentComponent: Component<any> | Array<Component<any>>,
-        element: SFCElement<any>,
-        container: Element,
-        callback?: () => void
-    ): void;
+export interface _model {
+  name: string;
+  reducers: any;
+}
+export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  to: H.LocationDescriptor;
+  replace?: boolean;
+}
+export interface NavLinkProps extends LinkProps {
+  activeClassName?: string;
+  activeStyle?: React.CSSProperties;
+  exact?: boolean;
+  strict?: boolean;
+  isActive?(match: match<any>, location: H.Location): boolean;
+  location?: H.Location;
 }
 
-  /**
-   * This method is used to create and inject a model.
-   * @param options 
-   */
-  function model(options:model):_model
-  /**
-   * connect connects your React component to your Redux store
-   */
-  const connect:Connect
-  //The actions object contains both your Redux actions and reducers.
-  const actions:Actions
-  //Add a hook to monitor actions that have been dispatched.
-  const hook:(subscriber:()=>any)=>any
-  //mirror.defaults is a pretty intuitive API, you use it to configure your Mirror app.
-  const defaults:(options:defaultOptions)=>void
-  //render is an enhanced ReactDOM.render, it starts your Mirror app.
-  const render: Renderer;
-  // Mirror uses react-router@4.x, so if you're from react-router 2.x/3.x, you should checkout the Migrating from v2/v3 to v4 Guide.
-  class Router<State> extends React.Component<ConnectedRouterProps<State>> {}
-
-  class Link extends React.Component<LinkProps, any> {}
-  
-  class NavLink extends React.Component<NavLinkProps, any> {}
-  
-  class Redirect extends React.Component<RedirectProps, any> { }
-
-  class Route<T extends RouteProps = RouteProps> extends React.Component<T, any> { }
-
-  class Prompt extends React.Component<PromptProps, any> { }
-  
-  function withRouter<P extends RouteComponentProps<any>>(component: React.ComponentType<P>): React.ComponentClass<Omit<P, keyof RouteComponentProps<any>>>;
-  
-  function withRouter<P, TFunction extends React.ComponentClass<P>>(target: TFunction): TFunction;
+export interface SwitchProps {
+  children?: React.ReactNode;
+  location?: H.Location;
 }
+
+export interface RedirectProps {
+  to: H.LocationDescriptor;
+  push?: boolean;
+  from?: string;
+  path?: string;
+  exact?: boolean;
+  strict?: boolean;
+}
+
+export interface RouteComponentProps<P> {
+  match: match<P>;
+  location: H.Location;
+  history: H.History;
+  staticContext?: any;
+}
+
+export interface RouteProps {
+  location?: H.Location;
+  component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+  render?: ((props: RouteComponentProps<any>) => React.ReactNode);
+  children?: ((props: RouteComponentProps<any>) => React.ReactNode) | React.ReactNode;
+  path?: string;
+  exact?: boolean;
+  strict?: boolean;
+}
+
+export interface PromptProps {
+  message: string | ((location: H.Location) => string | boolean);
+  when?: boolean;
+}
+
+export interface LocationActionPayload {
+  method: string;
+  args?: any[];
+}
+
+export interface RouterAction {
+  type: '@@router/CALL_HISTORY_METHOD';
+  payload: LocationActionPayload;
+}
+
+export interface routeActions {
+  push(location: any, state?: any): RouterAction;
+  replace(location: any, state?: any): RouterAction;
+  go(n: number): RouterAction;
+  goBack(): RouterAction;
+  goForward(): RouterAction;
+}
+
+export interface ConnectedRouterProps<State> {
+  store?: any;
+  history?: H.History;
+}
+
+export interface Actions {
+  routing: routeActions;
+  [propName: string]: any;
+}
+
+export interface defaultOptions {
+  initialState?: {};
+  historyMode?: string;
+  middlewares: any[];
+  reducers: {};
+  addEffect(name: string, handler: () => any): any;
+}
+
+export interface Renderer {
+  // Deprecated(render): The return value is deprecated.
+  // In future releases the render function's return type will be void.
+  <T extends Element>(
+    element: React.DOMElement<React.DOMAttributes<T>, T>,
+    container: Element | null,
+    callback?: () => void
+  ): T;
+
+  (
+    element: Array<React.DOMElement<React.DOMAttributes<any>, any>>,
+    container: Element | null,
+    callback?: () => void
+  ): Element;
+
+  (
+    element: React.SFCElement<any> | Array<React.SFCElement<any>>,
+    container: Element | null,
+    callback?: () => void
+  ): void;
+
+  <P, T extends React.Component<P, React.ComponentState>>(
+    element: React.CElement<P, T>,
+    container: Element | null,
+    callback?: () => void
+  ): T;
+
+  (
+    element: Array<React.CElement<any, React.Component<any, React.ComponentState>>>,
+    container: Element | null,
+    callback?: () => void
+  ): React.Component<any, React.ComponentState>;
+
+  <P>(
+    element: React.ReactElement<P>,
+    container: Element | null,
+    callback?: () => void
+  ): React.Component<P, React.ComponentState> | Element | void;
+
+  (
+    element: Array<React.ReactElement<any>>,
+    container: Element | null,
+    callback?: () => void
+  ): React.Component<any, React.ComponentState> | Element | void;
+
+  (
+    parentComponent: React.Component<any> | Array<React.Component<any>>,
+    element: React.SFCElement<any>,
+    container: Element,
+    callback?: () => void
+  ): void;
+}
+
+/**
+ * This method is used to create and inject a model.
+ */
+export function model(options: model): _model;
+/**
+ * connect connects your React component to your Redux store
+ */
+export const connect: Connect;
+export const actions: Actions;
+export function hook(): (subscriber: () => any) => any;
+export function defaults(): (options: defaultOptions) => void;
+export const render: Renderer;
+export class Router<State> extends React.Component<ConnectedRouterProps<State>> { }
+export class Link extends React.Component<LinkProps, any> { }
+export class NavLink extends React.Component<NavLinkProps, any> { }
+export class Redirect extends React.Component<RedirectProps, any> { }
+export class Route<T extends RouteProps = RouteProps> extends React.Component<T, any> { }
+export class Prompt extends React.Component<PromptProps, any> { }
+export function withRouter<P extends RouteComponentProps<any>>(component: React.ComponentType<P>): React.ComponentClass<Omit<P, keyof RouteComponentProps<any>>>;
+export function withRouter<TFunction extends React.ComponentClass<any>>(target: TFunction): TFunction;
--- mirrorx_test.ts
+++ mirrorx_test.ts
@@ -1,26 +1,24 @@
-import { Component, ReactElement } from 'react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as mirror from 'mirrorx';
-import { render,actions,withRouter,Link,NavLink,Router,Route,connect,Redirect} from 'mirrorx'
-
+import mirror = require('mirrorx');
+/**
+ * add mirror.model.tests
+ */
 mirror.model({
-    name:'app',
+    name: 'app',
     initialState: 0,
-    reducers:{
-        increment(state:any) { return state + 1 },
-        decrement(state:any) { return state - 1 }
+    reducers: {
+        increment(state: any) { return state + 1; },
+        decrement(state: any) { return state - 1; }
     },
-    effects:{
+    effects: {
         async incrementAsync() {
             await new Promise((resolve, reject) => {
-              setTimeout(() => {
-                resolve()
-              }, 1000)
-            })
-            actions.app.increment()
+                setTimeout(() => {
+                    resolve();
+                }, 1000);
+            });
+            mirror.actions.app.increment();
         }
     }
 });
-
-
